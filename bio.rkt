57fe4edc142ea08bffb9453bf6d418e6394b461d
PThayalan <61489313+PThayalan@users.noreply.github.com>
2020-03-08 23:13:04 -0700

    Create bio.rkt

---
+++ bio.rkt
@@ -0,0 +1,11 @@
+#lang mc-coach/bio
+
+
+(bio
+ #:first-name "PAM"
+ #:profile-pic (triangle 80 'solid 'teal )
+ #:coach-story "I joined ThoughtStem to learn basic coding so i can understand what it's all about
+and to share my knowledge with my kids. Along the way, I realised that i shared their passion to teach children all about coding.
+Children are like sponges; they absorb knowledge in story-form easily and retain that memory. By teaching them logical thinking
+and applying that to coding, the kids are learning a life skill that will aid them as they grow older. I hope that
+my Coach story will inspire both young and old, and that learning has no age limit.")
